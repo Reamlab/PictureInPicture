383f8bc71a9386f979e18f981bfc870ec6de11e4
Razuvaev Ilia <i.razuvaev@rambler-co.ru>
2017-02-13 18:53:09 +0300

    Initial Commit

---
+++ PicInPic/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PicInPic
+//
+//  Created by i.razuvaev on 13.02.17.
+//  Copyright © 2017 i.razuvaev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ PicInPic/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PicInPic
+//
+//  Created by i.razuvaev on 13.02.17.
+//  Copyright © 2017 i.razuvaev. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
